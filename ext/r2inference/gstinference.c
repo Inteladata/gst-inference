5957c796c1b6b431914ad147f656214fd9948e83
C.L. Hammond <clh@CLs-MBP-2.attlocal.net>
2023-11-08 11:26:26 -0500

    wip

--- ext/r2inference/gstinference.c
+++ ext/r2inference/gstinference.c
@@ -106,14 +106,14 @@ out:
   return ret;
 }
 
-// GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
-//     GST_VERSION_MINOR,
-//     inference,
-//     "Infer pre-trained model on incomming image frames on a variety of architectures and different backends",
-//     plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
-
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    "foobar",
+    inference,
     "Infer pre-trained model on incomming image frames on a variety of architectures and different backends",
-    plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)    
+    plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
+
+// GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+//     GST_VERSION_MINOR,
+//     "foobar",
+//     "Infer pre-trained model on incomming image frames on a variety of architectures and different backends",
+//     plugin_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)    
